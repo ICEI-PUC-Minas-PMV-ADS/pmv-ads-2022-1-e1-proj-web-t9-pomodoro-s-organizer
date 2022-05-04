5e7594deeed5831241cd8cdd62acf8a528ffac72
LukePrado <lucianor.prado@hotmail.com>
2022-05-04 09:02:26 -0300

    Funcionalidade de exibir form

--- src/agenda-frame.js
+++ src/agenda-frame.js
@@ -1,1 +1,22 @@
+var addAt = document.getElementById('btAdd');
 
+/*const boxAdd = "<form>	
+		Atividade<input type = "text">
+		Hora Inicio<input type="time">
+		Hora Fim <input type="time">	
+		</form>";/*
+
+/*var agenda = {
+	[nome: "Teste",
+	hrInicio: 1234,
+	hrFim: 1234
+	]
+	
+}*/
+
+addAt.addEventListener('click', 
+	function (){
+		this.hidden = true;
+		document.getElementById('addTable').hidden = false;
+			
+	});
