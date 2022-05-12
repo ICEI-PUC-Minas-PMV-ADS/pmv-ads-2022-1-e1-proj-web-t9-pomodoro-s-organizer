6b5589b0325ded5804471ee3c94e709629b24cc9
LukePrado <lucianor.prado@hotmail.com>
2022-05-12 14:21:04 -0300

    apresentação dinâmica da tabela da agenda

--- src/agenda-frame.js
+++ src/agenda-frame.js
@@ -1,21 +1,19 @@
 /*********************************************************************/
 /*******Variárives Globais********************************************/
 
-var addAt = document.getElementById('btAdd');//btn incluir atividade
-var addConf = document.getElementById('btConf');//btn confirmar inclusão
-var addCanc = document.getElementById('btCanc');//btn cancelar inclusão
-var bdAgenda = [];
-//var dataHj = document.getElementById('diaAtual')
+var addAt = document.getElementById('btAdd');//bt incluir atividade
+var addConf = document.getElementById('btConf');//bt confirmar inclusão
+var addCanc = document.getElementById('btCanc');//bt cancelar inclusão
+var bdAgenda = []; // banco de dados da agenda
+
 
 /*********************************************************************/
 /*******Funcionalidades dos Botões************************************/
 //Acção para pŕeencher da data atual qdo carregar a página.
-
 window.addEventListener('pageshow', (event) => {
 	hj = new Date;
 	document.getElementById('diaAtual').textContent = `Agenda dia ${hj.getDate()}
-	 / ${hj.getMonth()+1} / ${hj.getFullYear()}`;
-	 
+	 / ${hj.getMonth()+1} / ${hj.getFullYear()}`;	 
 });
 
 // Ação do Botão para incluir atividade
@@ -41,13 +39,13 @@ addConf.addEventListener('click',
 		tempItem = {
 		nome:   document.getElementById('ativNome').value,
 		inicio: {
-			horaInicio:  hInicio,
-			minutoInicio: mInicio,
+			hora:  hInicio,
+			minuto: mInicio,
 			//tempoInicio: tInicio,
 		},
 		fim: {
-			horaFim: hFim,
-			minutoFim: mFim,
+			hora: hFim,
+			minuto: mFim,
 			//tempoFim: tFim,
 		},
 		status: 0,
@@ -55,7 +53,9 @@ addConf.addEventListener('click',
 		};
 		bdAgenda[bdAgenda.length] = tempItem;
 		alert("Inclusão de dados realizada");
-		document.getElementById("ativNome").value = null;	
+		document.getElementById("ativNome").value = null; // LIMPA O INPUT	
+		ExibeAgenda ();
+		
 	});
 
 //Ação do Botão Cancelar (cancela a inclusão de dados no formulário)	
@@ -65,14 +65,44 @@ addCanc.addEventListener('click',
 		addAt.hidden = false;		
 	});
 	
-/*********************************************************************/
-/*******Local STORAGE************************************/	
-// JSON com o banco de dados da agenda.	
-var bdAgenda =[ //Exemplo:
-	/*{nome: "Teste",
-	hrInicio: "",
-	hrFim: "",
-	status: 0, //0 não iniciado, 1 andamento, 2 conclúído
-	progress: 0.75 //percentual de progresso
-	},*/
-	];
+//Função para exibir tabela da agenda:
+function ExibeAgenda () {
+	var tabela = document.getElementById('tabelaAgenda');
+	var textoHTML = `
+	<table class="table">
+		  <thead>
+			<tr>
+			  <th scope="col">#</th>
+			  <th scope="col">Descrição</th>
+			  <th scope="col">Inicio</th>
+			  <th scope="col">Fim</th>
+			</tr>
+		  </thead>
+		  <tbody>
+	`; //Preencher o HEADER da Tabela.
+	
+	if (bdAgenda.length == 0){//se não tem atidivada programada	
+		tabela.innerHTML = textoHTML + `
+		<tr>		  
+			<td colspan="4"><i>Nenhuma Atividade Cadastrada</i></td>
+		</tr>
+		`;//preenche uma linha dizendo que não tem atividade programada
+	} else {
+		
+		for (var i = 0; i < bdAgenda.length; i++){
+			console.log("chegou no FOR");
+			textoHTML +=`
+						<tr>
+							  <th scope="row">${i+1}</th>
+							  <td>${bdAgenda[i].nome}</td>
+							  <td>${new Intl.NumberFormat('en-IN', { minimumIntegerDigits: 2 }).format(bdAgenda[i].inicio.hora) } : ${new Intl.NumberFormat('en-IN', { minimumIntegerDigits: 2 }).format(bdAgenda[i].inicio.minuto)}</td>
+							  <td>${new Intl.NumberFormat('en-IN', { minimumIntegerDigits: 2 }).format(bdAgenda[i].fim.hora) } : ${new Intl.NumberFormat('en-IN', { minimumIntegerDigits: 2 }).format(bdAgenda[i].fim.minuto)}</td>
+							 						  
+						</tr>							
+						`;						
+		}//fim do for
+		tabela.innerHTML = textoHTML; //imprime a tabela.
+	} //fim do else
+		
+}//fim da função ExibeAgenda
+
