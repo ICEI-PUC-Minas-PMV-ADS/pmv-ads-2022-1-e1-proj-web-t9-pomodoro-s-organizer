36875c2c1a0ce9a8f202beef7f7db9d5edec295b
LukePrado <lucianor.prado@hotmail.com>
2022-05-26 17:32:49 -0300

    Reformulado front da agenda. Incluido botões e progress bar

--- src/agenda-frame.js
+++ src/agenda-frame.js
@@ -134,7 +134,7 @@ function mintemp(){
 function ExibeAgenda () {
 	var tabela = document.getElementById('tabelaAgenda');
 	var textoHTML = `
-	<table class="table">
+	<table class = "table table-bordered">
 		  <thead>
 			<tr>
 			  <th scope="col">#</th>
@@ -152,6 +152,8 @@ function ExibeAgenda () {
 		<tr>		  
 			<td colspan="4"><i>Nenhuma Atividade Cadastrada</i></td>
 		</tr>
+		</tbody>
+		</table>
 		`;//preenche uma linha dizendo que não tem atividade programada
 	} else {
 		
@@ -159,14 +161,62 @@ function ExibeAgenda () {
 		for (var i = 0; i < bdAgenda.length; i++){
 			textoHTML +=`
 						<tr>
-							  <th scope="row">${i+1}</th>
-							  <td>${bdAgenda[i].nome}</td>
+							  <th scope="row" rowspan = "2" class="align-middle">${i+1}</br>
+							  </th>
+							  <td class="align-middle"><b>${bdAgenda[i].nome}</b></td>
 							  <td>${new Intl.NumberFormat('en-IN', { minimumIntegerDigits: 2 }).format(bdAgenda[i].inicio.hora) } : ${new Intl.NumberFormat('en-IN', { minimumIntegerDigits: 2 }).format(bdAgenda[i].inicio.minuto)}</td>
 							  <td>${new Intl.NumberFormat('en-IN', { minimumIntegerDigits: 2 }).format(bdAgenda[i].fim.hora) } : ${new Intl.NumberFormat('en-IN', { minimumIntegerDigits: 2 }).format(bdAgenda[i].fim.minuto)}</td>
-							 						  
-						</tr>							
+							  
+						</tr>
+						<tr>
+							
+							<td class="align-middle"> 
+							
+								<form>
+														
+								 <input type="range" id = "statusbar${i}" name = "${i}" min = "0" max = "100" step = "1" value = "${bdAgenda[i].progress}">
+								</form>
+							
+							</td>
+							
+							<td colspan="3">
+								<div class = "btn-group">
+									<button type="button" class="btn btn-outline-secondary">
+										<span id = "btn-play-${i}" name = "${i}">
+											<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 16">
+												<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+												<path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z"/>
+											</svg>	
+										</span>
+									
+									<button type="button" class="btn btn-outline-secondary">
+										<span id = "btn-exc-${i}" name = "${i}">
+											<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+												<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
+												<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
+											</svg>
+										 </span>
+									</button>
+									
+										
+									<button type="button" class="btn btn-outline-secondary">
+										<span id = "btn-alt-${i}" name = "${i}">
+											<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16">
+												path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"/>
+												<path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"/>
+											</svg>
+										</span>
+									</button>
+									
+								</div>
+								
+							</td>
+						</tr>
+		
 						`;						
 		}//fim do for
+		textoHTML +=`</tbody>
+						</table>`
 		tabela.innerHTML = textoHTML; //imprime a tabela.
 	} //fim do else
 		
